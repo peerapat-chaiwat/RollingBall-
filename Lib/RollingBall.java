73cd1a39d57ca57a722ff579a105e72359c1db30
peerapat-chaiwat <peerapat.chaiw@ku.th>
2026-07-03 15:54:40 +0700

    Version 2.00 upg

--- Lib/RollingBall.java
+++ Lib/RollingBall.java
@@ -1,10 +1,30 @@
 package Lib ;
 import java.awt.*;
 import javax.swing.*;
-public class RollingBall extends JPanel {
+import java.awt.event.*;
+
+public class RollingBall extends JPanel implements ActionListener {
+    int x = 100 ;
+    int angle = 0 ;
+    public RollingBall (){
+        Timer t = new Timer(50, this);
+        t.start();
+    }
     public void paintComponent(Graphics g){
-        g.drawOval(100, 90, 80, 80);
-        g.fillArc(100, 90, 80, 80, 0, 180);
+        super.paintComponent(g);
+        g.drawOval(x, 90, 80, 80);
+        g.fillArc(x, 90, 80, 80, angle, 180);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       x -= 2;
+       angle += 2;
+
+       if(x<-80) x = getWidth();
+       if (angle >= 360) angle = getWidth() ; 
+       repaint();
+
     }
 
     
